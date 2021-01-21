04e8be9f77ec9c8350c418d83b03b44cc53d0b54
jaimecostx <tmdcosta.17@gmail.com>
2021-01-21 19:12:23 +0000

    added clock, finished team page

--- HTML/js/jsfileequipa.js
+++ HTML/js/jsfileequipa.js
@@ -13,3 +13,23 @@ var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
 var collapseList = collapseElementList.map(function (collapseEl) {
   return new bootstrap.Collapse(collapseEl)
 })
+
+function FunctionAlert() {
+  alert("Este link redireciona para uma página externa!");
+}
+
+setInterval(function(){
+  const clock = document.querySelector(".display");
+  let time = new Date();
+  let sec = time.getSeconds();
+  let min = time.getMinutes();
+  let hr = time.getHours();
+  if (sec<10){
+    sec = '0'+sec;
+  }if (min<10){
+    min = '0'+min;
+  }if(hr<10){
+    hr = '0'+hr;
+  }
+  clock.textContent = hr + ":" + min + ":" + sec 
+});
